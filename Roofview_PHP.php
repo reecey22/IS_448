d95f9c14a88d6a28237f117e005bce3b6a4701b0
Shaniah Reece <104451377+reecey22@users.noreply.github.com>
2023-04-17 20:38:56 -0400

    Add files via upload

---
+++ Roofview_PHP.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head> <meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+
+	<title>Roofview Form Responses </title>
+	
+	<!-- This links to a stylesheet that gives basic formatting to your page; you do not need to modify this. -->
+
+	<link rel="stylesheet" type="text/css" href="./d4_css.css" title="style">
+	<!-- Style palet taking from: https://www.canva.com/colors/color-palettes/emerald-entrance/ -->	
+</head>
+
+<body>
+
+<?php 
+
+$loc = $loc_name = $headline = $desc = "";
+
+$loc_err = $loc_name_err = $headline_err = $desc_err = "";
+
+
+
+
+?>
+
+</body>
+</html>
+
